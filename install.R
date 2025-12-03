fc0557fb937b33a8eef2a8bcc59829b05e71cd27
Genevieve Kozak <59708405+gmkozak@users.noreply.github.com>
2025-12-03 11:11:14 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,1 @@
-if (!require("devtools")) install.packages("devtools")
-devtools::install_url("https://code.usgs.gov/fort/ecosystems-cgsim/-/archive/1.0.2/ecosystems-cgsim-1.0.2.zip")
-require(cgsim)
+install.packages("devtools")
